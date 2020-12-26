8310789d7aa1d55f3d87ba957a128ea5a6608b88
Elmakan Sytems <74911167+elmakansystems@users.noreply.github.com>
2020-12-27 00:03:25 +0200

    Update main.js

--- furnitre-town/scripts/main.js
+++ furnitre-town/scripts/main.js
@@ -3,6 +3,7 @@ window.onload = ()=>{
     const images = document.querySelectorAll(".logo-cont img")
     const mainImage = document.querySelector(".main-logo img")
     const brands = document.querySelectorAll(".logo-cont")
+    mainImage.click()
     setTimeout(() => {
     let i =0
         let addClass = setInterval(() => {
@@ -32,9 +33,9 @@ setTimeout(() => {
 
     setTimeout(() => {
         const music = document.getElementById("bgMusic")
-         music.click()
+        music.click()
         music.play()
-    }, 2000)
+    }, 10000)
     
     
 }
